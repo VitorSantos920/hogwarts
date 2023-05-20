b4262cfc18b8dd27a61fd91f2704fda38e3e68f0
Vitor Santos <pireshugo737@gmail.com>
2023-05-20 16:20:50 -0300

    feat: responsive header component | style: BEM css methodology | refactor

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,29 +1,42 @@
 import '../../styles/components/Header.scss';
 import { FiMenu } from 'react-icons/fi';
+import { useState } from 'react';
 
 export function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <header className="header flex">
-            <div className="container flex">
-                <a href="#" className="logo">
+            <div className="header__container flex">
+                <a href="#" className="header__logo">
                     HW
                 </a>
-                <nav className="menu">
-                    <ul className="menu-itens flex">
-                        <li className="item">
-                            <a href="#">Home</a>
+
+                <nav className="header__menu">
+                    <ul
+                        className={`header__menu-itens flex ${
+                            isOpen && 'open'
+                        }`}
+                    >
+                        <li className="header__item">
+                            <a href="https://youtube.com">Home</a>
                         </li>
-                        <li className="item">
+                        <li className="header__item">
                             <a href="#">História</a>
                         </li>
-                        <li className="item">
+                        <li className="header__item">
                             <a href="#">Casas</a>
                         </li>
-                        <li className="item">
+                        <li className="header__item">
                             <a href="#">Dumbledore</a>
                         </li>
                     </ul>
+                </nav>
 
+                <nav
+                    className={`header__menu--toggle ${isOpen && 'open'}`}
+                    onClick={() => setIsOpen(!isOpen)}
+                >
                     <FiMenu className="hidden-menu" size={52} />
                 </nav>
             </div>
