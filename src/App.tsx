24f61cc016c589aa90603a8941dc8ed8fd817b3f
Vitor Santos <pireshugo737@gmail.com>
2023-05-21 03:22:40 -0300

    feat: home component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import { Header } from "./components/Header";
+import { Header } from './components/Header';
+import { FiChevronsDown } from 'react-icons/fi';
+import './styles/components/Home.scss';
 
 function App() {
     return (
-        <Header />
-      )
+        <div className="App">
+            <Header />
+            <main className="main">
+                <section className="home flex">
+                    <h1 className="title">Hogwarts</h1>
+                    <h2>“Draco Dormiens Nunquam Titillandus.”</h2>
+
+                    <a href="#">
+                        <FiChevronsDown className="arrow-down" size={72} />
+                    </a>
+                </section>
+            </main>
+        </div>
+    );
 }
 
 export default App;
