37472aafbd9847a37126d1f965f297b042dd932e
Vitor Santos <pireshugo737@gmail.com>
2023-05-22 00:55:46 -0300

    :sparkles: Add: history section
    add history section structure to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Header } from './components/Header';
 import { FiChevronsDown } from 'react-icons/fi';
-import { Home } from './styles/components/Home';
+import { Home } from './styles/Home';
+import { ContainerHistory, History } from './styles/History';
+import { Button } from './components/Button';
 
 function App() {
     return (
@@ -13,10 +15,35 @@ function App() {
                         “Draco Dormiens Nunquam Titillandus.”
                     </h2>
 
-                    <a href="#">
+                    <a href="#history">
                         <FiChevronsDown className="home__arrowDown" size={72} />
                     </a>
                 </Home>
+
+                <History className="history flex" id="history">
+                    <ContainerHistory className="history__container flex">
+                        <h3 className="history__title">
+                            Sua <br /> História
+                        </h3>
+                        <div className="divider"></div>
+                        <div className="history__about">
+                            <p className="history__description">
+                                “Hogwarts foi fundada há mais de 1000 anos,
+                                pelos quatro maiores bruxos e bruxas da época:
+                                Godric Grifinória, Helga Lufa-Lufa, Rowena
+                                Ravenclaw e Salazar Sonserina. Três dos
+                                fundadores viviam em perfeita harmonia, mas
+                                Salazar Sonserina queria ser mais seletivo em
+                                relação aos alunos admitidos em Hogwarts. Ele
+                                achava que o Ensino da Magia devia ser somente
+                                para famílias inteiramente mágicas, em outras
+                                palavras, sangues puros...” (MCGONAGALL,
+                                Minerva).
+                            </p>
+                            <Button house={'default'}>Leia Mais</Button>
+                        </div>
+                    </ContainerHistory>
+                </History>
             </main>
         </div>
     );
