51b0bf1dcd0e5bda4a9fdac60e682158728c0735
Vitor Santos <pireshugo737@gmail.com>
2023-05-27 09:37:23 -0300

    :lipstick: Houses styles
    add houses section styles

---
+++ src/styles/Houses.ts
@@ -0,0 +1,18 @@
+import { stitches } from '../../stitches.config';
+
+const { styled } = stitches;
+
+export const Houses = styled('section', {
+    '& > div': {
+        flexDirection: 'column',
+
+        '.houses__title': {
+            fontSize: '$title',
+        },
+
+        '.houses__description': {
+            textAlign: 'center',
+            width: '72.5%',
+        },
+    },
+});
