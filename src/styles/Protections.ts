de645381ea2a9bcca82e2793b4c3cd0c24283bc0
Vitor Santos <pireshugo737@gmail.com>
2023-06-07 02:37:10 -0300

    :lipstick: Styles: protections section
    add styles to protections section

---
+++ src/styles/Protections.ts
@@ -0,0 +1,9 @@
+import { stitches } from '../../stitches.config';
+
+const { styled } = stitches;
+
+export const Protections = styled('section', {
+    '.protections__description': {
+        marginTop: '$pSpace',
+    },
+});
